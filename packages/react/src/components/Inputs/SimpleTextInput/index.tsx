0c85b9ba11c2f2018476ec2993607ef5d82ddf10
natan-chmura <natan.chmura@inovanti.com.br>
2023-11-28 17:06:20 -0300

    fix: rename export of type SimpleInputText component

--- packages/react/src/components/Inputs/SimpleTextInput/index.tsx
+++ packages/react/src/components/Inputs/SimpleTextInput/index.tsx
@@ -3,20 +3,20 @@ import { Input } from '../common.styles'
 import { InputContainer } from './styles'
 import { Spaces } from '../../../types/styles'
 
-export interface InputProps extends ComponentProps<typeof Input> {
+export interface SimpleInputProps extends ComponentProps<typeof Input> {
   inputSize?: 'sm' | 'md'
   width?: Spaces
   gridAreaName?: string
 }
 
-export const SimpleTextInput = forwardRef<ElementRef<typeof Input>, InputProps>(
+export const SimpleTextInput = forwardRef<ElementRef<typeof Input>, SimpleInputProps>(
   (
     {
       inputSize = 'md',
       gridAreaName,
       width = 'full',
       ...props
-    }: InputProps,
+    }: SimpleInputProps,
     ref,
   ) => {
     return (
