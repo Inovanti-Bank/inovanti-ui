3c2279bdfeb0d8a914dbf470296bda3c7e0ee659
natan-chmura <natan.chmura@inovanti.com.br>
2023-12-13 09:31:15 -0300

    feat: change colors of CheckBox

--- packages/docs/src/stories/Checkbox.stories.tsx
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { AButton, Box, Checkbox, Text } from '@inovanti/react'
+import { AButton, Box, Checkbox, Text, light } from '@inovanti/react'
+import { ThemeProvider } from 'styled-components'
 
 export default {
   title: 'Form/Buttons/Checkbox',
@@ -17,28 +18,61 @@ export default {
       action: 'click',
     },
   },
-   decorators: [
-    (Story) => {
-      return (
-        <Box
-          as="label"
-          style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}
-        >
-          {Story()}
-          <Text size="sm">Aceitar termos de uso</Text>
-        </Box>
-      )
-    },
-  ],
+   
 } as Meta<typeof AButton>
 
-export const Primary: StoryObj<typeof AButton> = {}
+export const Primary: StoryObj<typeof AButton> = {
+    decorators: [
+        (Story) => {
+          return (
+            <Box
+              as="label"
+              style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}
+            >
+              {Story()}
+              <Text size="sm">Aceitar termos de uso</Text>
+            </Box>
+          )
+        },
+      ],
+}
 
 export const Disabled: StoryObj<typeof AButton> = {
     args: {
     disabled: true,
     },
+    decorators: [
+      (Story) => {
+        return (
+          <Box
+            as="label"
+            style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}
+          >
+            {Story()}
+            <Text size="sm">Aceitar termos de uso</Text>
+          </Box>
+        )
+      },
+    ],
 }
 
+export const LightMode: StoryObj<typeof AButton> = {
+    decorators: [
+    (Story) => {
+        return (
+        <ThemeProvider theme={light}>
+            
+        <Box
+          as="label"
+          style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem' }}
+          >
+          {Story()}
+          <Text size="sm">Aceitar termos de uso</Text>
+        </Box>
+          </ThemeProvider>
+      )
+    },
+  ],
+}
 
 
