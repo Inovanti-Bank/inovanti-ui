fbc7e2fabe412f2df99700af33ddda1aa17744c0
natan-chmura <natan.chmura@inovanti.com.br>
2023-12-19 14:25:12 -0300

    feat: Add prop Weight in Text component, change color and add prop small in switch

--- packages/react/src/components/Switch/styles.ts
+++ packages/react/src/components/Switch/styles.ts
@@ -1,31 +1,39 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { Root, Thumb } from '@radix-ui/react-switch'
 
-export const SwitchRoot = styled(Root)`
+type SwitchPros = {
+  small?: boolean
+}
+
+export const SwitchRoot = styled(Root) <SwitchPros>`
+${({ theme, small = false }) => css`
   all: unset;
-  width: 33px;
-  height: 22px;
+  width: ${small ? '26px' : '33px'};
+  height: ${small ? '17px' : '22px'};
   margin: 1px 10px 1px 0;
   cursor: pointer;
-  background-color: ${({ theme }) => theme.colors.gray500};
+  background-color: ${theme.title === 'light' ? theme.colors.gray300 : theme.colors.gray700};
   border-radius: 9999px;
   position: relative;
   &[data-state='checked'] {
-    background-color: ${({ theme }) => theme.colors.green300};
+    background-color: ${theme.colors.primary500};
   }
+  `};
 `
 
-export const SwitchThumb = styled(Thumb)`
+export const SwitchThumb = styled(Thumb) <SwitchPros>`
+${({ small = false }) => css`
   display: block;
-  width: 17px;
-  height: 17px;
+  width: ${small ? '13px' : '17px'};
+  height: ${small ? '13px' : '17px'};
   background-color: white;
   border-radius: 9999px;
   transition: transform 100ms;
   transform: translateX(2px);
   will-change: transform;
   &[data-state='checked'] {
-    transform: translateX(13px);
+    transform: translateX(${small ? '11px' : '13px'});
   }
+  `};
 `
--- packages/react/src/components/Text.tsx
+++ packages/react/src/components/Text.tsx
@@ -1,20 +1,22 @@
 import { ElementType, ReactNode } from 'react'
 import styled, { css } from 'styled-components'
-import { FontSizes } from '../types/styles'
+import { FontSizes, FontWeights } from '../types/styles'
 
 export interface TextProps {
   size?: FontSizes
   as?: ElementType
+  fontWeight?: FontWeights
   children: ReactNode
 }
 
 export const Text = styled.p<TextProps>`
-  ${({ theme, size = 'md' }) => css`
+  ${({ theme, size = 'md', fontWeight = 'regular' }) => css`
     font-family: ${theme.fonts.default};
     line-height: ${theme.lineHeights.base};
     margin: 0;
     color: ${theme.colors.textHigh};
     font-size: ${theme.fontSizes[size]};
+    font-weight: ${theme.fontWeights[fontWeight]};
   `};
 `
 
