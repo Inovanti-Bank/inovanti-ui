a86788affa3be15f82e47bea837fc0ea97e34819
natan-chmura <natan.chmura@inovanti.com.br>
2024-04-25 17:27:03 -0300

    feat: implementation of textPrefixInput in tailwind

--- packages/react/src/components/Inputs/TextPrefixInput/index.tsx
+++ packages/react/src/components/Inputs/TextPrefixInput/index.tsx
@@ -1,40 +1,58 @@
-import { ComponentProps, ElementRef, ReactNode, forwardRef } from 'react'
-import { Spaces } from '../../../types/styles'
+import { cn } from '@/utils/cn'
+import { SpaceType, resolveSpace } from '@inovanti/tokens'
+import { ComponentProps, ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 import { FormAlert, FormAlertBlank } from '../../FormAlert'
 import { Text } from '../../Text'
-import { Input, InputContainer, InputWithPrefixContainer, Prefix } from './styles'
 
-export interface TextPrefixInputProps extends Omit<ComponentProps<typeof Input>, 'prefix'> {
-  label: string
-  inputSize?: 'sm' | 'md'
+export interface TextPrefixInputProps extends Omit<ComponentPropsWithoutRef<'input'>, 'prefix'> {
+  label?: string
+  className?: string
   error?: string
-  width?: Spaces
-  gridAreaName?: string
+  $width?: SpaceType
+  $gridArea?: string
   prefix: string | ReactNode
 }
 
-export const TextPrefixInput = forwardRef<ElementRef<typeof Input>, TextPrefixInputProps>(
+export const TextPrefixInput = forwardRef<HTMLInputElement, TextPrefixInputProps>(
   (
     {
       label,
-      inputSize = 'md',
-      gridAreaName,
-      width = 'full',
+      className = '',
       error,
       prefix,
+      $width = '64',
+      $gridArea,
       ...props
     }: TextPrefixInputProps,
     ref,
   ) => {
     return (
-      <InputContainer width={width} style={{ gridArea: gridAreaName }}>
-        <Text $size="text-sm" className='mb-1' as="label">{label}</Text>
-        <InputWithPrefixContainer inputSize={inputSize}>
-        <Prefix>{prefix}</Prefix>
-        <Input type="text" ref={ref} {...props} />
-        </InputWithPrefixContainer>
+      <div
+        className={cn(
+          'flex flex-col justify-start mb-4',
+          `${resolveSpace($width)}`
+        )}
+        style={{ gridArea: $gridArea }}
+      >
+        {label &&
+          <Text $size="text-sm" className='mb-1' as="label">{label}</Text>
+        }
+        <div className={cn(
+          'input-with-prefix flex items-center w-full text-base bg-background text-gray-primary',
+          'rounded-sm px-3 py-2',
+          'border border-primary',
+          className
+        )}>
+        <span className='text-gray-secondary'>{prefix}</span>
+          <input
+            className='w-full cursor-text bg-transparent focus:outline-none disabled:cursor-not-allowed placeholder:text-gray-tertiary'
+            type="text"
+            ref={ref}
+            {...props}
+          />
+        </div>
         {error ? <FormAlert>{error}</FormAlert> : <FormAlertBlank />}
-      </InputContainer>
+      </div>
     )
   },
 )
--- packages/react/src/components/Inputs/TextPrefixInput/styles.ts
+++ packages/react/src/components/Inputs/TextPrefixInput/styles.ts
@@ -1,42 +1,11 @@
 import styled, { css } from 'styled-components'
-import { Spaces } from '../../../types/styles'
-
-export interface ContainerProps {
-  width?: Spaces
-}
 
 export type InputProps = {
   inputSize?: 'sm' | 'md'
 }
 
-export const InputContainer = styled.div<ContainerProps>`
-  ${({ theme, width = 'full' }) => css`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    margin-bottom: 1rem;
-    width: ${theme.space[width || 'full']};
-
-    @media (max-width: 768px) {
-      width: ${theme.space.full};
-    }
-  `};
-`
-
 export const InputWithPrefixContainer = styled.div<InputProps>`
-    ${({ theme, inputSize = 'md' }) => css`
-        display: flex;
-        align-items: center;
-        width: ${theme.space.full};
-        font-size: ${
-        // @ts-expect-error TODO: arrumar isso daqui ein
-        theme.fontSizes[inputSize]
-        };
-        background-color: ${theme.colors.white};
-        color: ${theme.colors.textHigh};
-        border-radius: ${theme.radii['rounded-sm']};
-        font-family: ${theme.fonts.default};
-        padding: ${theme.space[inputSize === 'sm' ? 2 : 3]};
+    ${({ theme, }) => css`
 
         &:has(input:focus) {
             border: 1px ${theme.colors.primary700} solid;
@@ -47,13 +16,6 @@ export const InputWithPrefixContainer = styled.div<InputProps>`
             color: ${theme.colors.gray300};
         }
     `};
-`
-
-export const Prefix = styled.span`
-    ${({ theme }) => css`
-        color: ${theme.colors.gray800};
-    `};
-
 `
 
 export const inputStyles = css`
