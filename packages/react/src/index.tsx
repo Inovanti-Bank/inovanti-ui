d9e347aae201c51795e856c3f89e43195e6de47a
natan-chmura <natan.chmura@inovanti.com.br>
2024-04-25 10:51:19 -0300

    feat: rude reestruturation in space to support tailwind

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -44,3 +44,10 @@ export * from './types/styles.d'
 
 import './styles/globals.css'
 
+// You: WTF? Why we have this comments here?
+// Me: Check this: https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
+// Me: Doing this, the consumer of the design system doesn't necessary needs to have tailwind installed, but, of course, it will be a limited experience
+
+// text-sizes: "text-xs" | "text-sm" | "text-base" | "text-lg" | "text-xl" | "text-2xl" | "text-3xl" | "text-4xl" | "text-5xl" | "text-6xl" | "text-7xl" | "text-8xl" | "text-9xl"
+// font-weights: "font-thin" | "font-extralight" | "font-light" | "font-normal" | "font-medium" | "font-semibold" | "font-bold" | "font-extrabold" | "font-black"
+// widths: "w-px" | "w-0.5" | "w-1" | "w-1.5" | "w-2" | "w-2.5" | "w-3" | "w-3.5" | "w-4" | "w-5" | "w-6" | "w-7" | "w-8" | "w-9" | "w-10" | "w-11" | "w-12" | "w-14" | "w-16" | "w-20" | "w-24" | "w-28" | "w-32" | "w-36" | "w-40" | "w-44" | "w-48" | "w-52" | "w-56" | "w-60" | "w-64" | "w-72" | "w-80" | "w-96" | "w-[27rem]" | "w-[30rem]" | "w-auto" | "w-1/2" | "w-1/3" | "w-2/3" | "w-1/4" | "w-2/4" | "w-3/4" | "w-1/5" | "w-2/5" | "w-3/5" | "w-4/5" | "w-1/6" | "w-2/6" | "w-3/6" | "w-4/6" | "w-5/6" | "w-1/12" | "w-2/12" | "w-3/12" | "w-4/12" | "w-5/12" | "w-6/12" | "w-7/12" | "w-8/12" | "w-9/12" | "w-10/12" | "w-11/12" | "w-full" | "w-screen" | "w-svw" | "w-lvw" | "w-dvw" | "w-min" | "w-max" | "w-fit"
