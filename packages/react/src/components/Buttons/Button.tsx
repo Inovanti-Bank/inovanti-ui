062a3db9516a0b56c579b2c84e0a7ac5bc283cb9
RobertoInov <roberto.souza@inovanti.com.br>
2025-01-22 12:20:05 -0300

    fix: fixed z-index select and buttons

--- packages/react/src/components/Buttons/Button.tsx
+++ packages/react/src/components/Buttons/Button.tsx
@@ -19,7 +19,7 @@ export const buttonStyles = cn(
   'disabled:bg-none disabled:shadow-none disabled:transition-none',
   'disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-700',
   'dark:disabled:bg-gray-700 dark:disabled:text-gray-200',
-  'before:ease relative overflow-hidden border shadow-2xl hover:border-gray-100 border-[1.5px]  hover:box-border box-border dark:hover:border-gray-700 transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-40 before:duration-1000 hover:shadow-primary/50 hover:before:-translate-x-40'
+  'before:ease  overflow-hidden border shadow-2xl hover:border-gray-100 border-[1.5px]  hover:box-border box-border dark:hover:border-gray-700 transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-40 before:duration-1000 hover:shadow-primary/50 hover:before:-translate-x-40'
 );
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
@@ -30,7 +30,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        <div className='z-10 relative'>{children}</div>
+        <div >{children}</div>
       </button>
     );
   }
